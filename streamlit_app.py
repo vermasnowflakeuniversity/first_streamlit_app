8c49ccc1e3b28259f81fbae12e55cbd19e779d45
vermasnowflakeuniversity <139138700+vermasnowflakeuniversity@users.noreply.github.com>
2023-07-19 15:20:04 +0530

    Update streamlit_app.py
    error solving

--- streamlit_app.py
+++ streamlit_app.py
@@ -60,4 +60,4 @@ streamlit.dataframe(my_data_rows)
 add_my_fruit=streamlit.text_input("What fruit would you like to add?","jackfruit")
 streamlit.write("thank you for adding ",add_my_fruit)
   
-my_cur.execute("insert into pc_rivery_db.public.fruit_load_list values('from streamlit')")
+my_cur.execute("insert into pc_rivery_db.public.fruit_load_list values ('from streamlit')")
