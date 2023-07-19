3b789905e2c56c634669dfacbf135fc7ccc58b09
vermasnowflakeuniversity <139138700+vermasnowflakeuniversity@users.noreply.github.com>
2023-07-19 16:16:52 +0530

    Update streamlit_app.py
    made all the formating

--- streamlit_app.py
+++ streamlit_app.py
@@ -50,19 +50,33 @@ try:
 except URLError as e:
   streamlit.error()
 
-streamlit.stop()
-
-# working with snowflake-connector-python module
-# import snowflake.connector
-
-my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
-my_cur = my_cnx.cursor()
-my_cur.execute("SELECT * from pc_rivery_db.public.fruit_load_list")
-my_data_rows = my_cur.fetchall()
 streamlit.header("The fruit load list contains:")
-streamlit.dataframe(my_data_rows)
+# snowflake related function
+def get_fruit_load_list(my_cnx):
+  with my_cnx.cursor() as my_cur:
+    my_cur.execute("SELECT * FROM pc_rivery_db.public.fruit_load_list")
+    return my_cur.fetchall()
 
-add_my_fruit=streamlit.text_input("What fruit would you like to add?","jackfruit")
-streamlit.write("thank you for adding ",add_my_fruit)
+# add a button to load the fruit
+if streamlit.button('Get Fruit Load List'):
+  my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
+  my_data_rows=get_fruit_load_list(my_cnx)
+  streamlit.dataframe(my_data_rows)
+
+
+# function to allow user to add a fruit to the list
+def insert_row_snowflake(new_fruit):
+  with my_cnx.cursor() as my_cur:
+    my_cur.execute(f"insert into pc_rivery_db.public.fruit_load_list values ({new_fruit})")
+    streamlit.write("thank you for adding ",new_fruit)
+
+
+add_my_fruit=streamlit.text_input("What fruit would you like to add ?")
+
+if add_my_fruit:
+  if streamlit.button('Add a fruit to the list'):
+    my_cnx=snowflake.connector.connect(**streamlit.secrets["snowflake"])
+    back_from_function=insert_row_snowflake(add_my_fruit)
+    streamlit.text(back_from_function)
   
-my_cur.execute("insert into pc_rivery_db.public.fruit_load_list values ('from streamlit')")
+
