a4f49f055f86e2681a5d19ae39df1ef475518fc9
vermasnowflakeuniversity <139138700+vermasnowflakeuniversity@users.noreply.github.com>
2023-07-19 11:15:58 +0530

    Update streamlit_app.py
    added streamlit text_input and modified the get request

--- streamlit_app.py
+++ streamlit_app.py
@@ -31,9 +31,13 @@ streamlit.dataframe(fruits_to_show)
 
 
 streamlit.header("Fruityvice Fruit Advice!")
+
+fruit_choice = streamlit.text_input('What fruit would you like information about?','Kiwi')
+streamlit.write('The user entered ', fruit_choice)
+
 # request module
 import requests
-fruityvice_response = requests.get("https://fruityvice.com/api/fruit/"+"kiwi")
+fruityvice_response = requests.get("https://fruityvice.com/api/fruit/"+str(fruit_choice).lower())
 # streamlit.text(fruityvice_response.json())
 
 # write your own comment -what does the next line do? 
