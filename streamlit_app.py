5426a22b70f7e0d2c9b2081669a912b5f0c2a415
vermasnowflakeuniversity <139138700+vermasnowflakeuniversity@users.noreply.github.com>
2023-07-19 15:04:53 +0530

    Update streamlit_app.py
    added another input field

--- streamlit_app.py
+++ streamlit_app.py
@@ -57,4 +57,7 @@ my_data_rows = my_cur.fetchall()
 streamlit.header("The fruit load list contains:")
 streamlit.dataframe(my_data_rows)
 
+add_my_fruit=streamlit.text_input("What fruit would you like to add?","jackfruit")
+streamlit.text("thank you for adding ",add_my_fruit)
+  
 
